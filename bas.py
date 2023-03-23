12bf244a7f3f4a7006dad3ad3b287778d4e1fb4a
Mustafa <abou08923@gmail.com>
2023-03-23 10:14:33 +0100

    finished working one mode version

--- bas.py
+++ bas.py
@@ -88,41 +88,41 @@ ser = serial.Serial('COM3', 9600)  # replace 'COM3' with the name of your serial
 def play_sound(note):
     if note == "one":
         # play a note when touch sensor is touched
-        playsound('banana-piano/wav-piano-sound/fart.mp3')#dit hoort c4 te zijn
+        playsound('wav-piano-sound/fart.mp3')#dit hoort c4 te zijn
         print("Note played!")
     elif note == "two":
         # play a note when touch sensor is touched
-        playsound('banana-piano/wav-piano-sound/c-4.mp3')#dit hoort c-4 te zijn
+        playsound('wav-piano-sound/c-4.mp3')#dit hoort c-4 te zijn
         print("Note played2!")
     elif  note == "three":
-        playsound('banana-piano/wav-piano-sound/d4.mp3')#dit hoort d4 te zijn
+        playsound('wav-piano-sound/d4.mp3')#dit hoort d4 te zijn
         print("Note played3!")
     elif  note == "four":
-        playsound('banana-piano/wav-piano-sound/d-4.mp3')#d-4
+        playsound('wav-piano-sound/d-4.mp3')#d-4
         print("Note played4!")
     elif  note == "five":
-        playsound('banana-piano/wav-piano-sound/e4.mp3')#e4
+        playsound('wav-piano-sound/e4.mp3')#e4
         print("Note played5!")
     elif  note == "six":
-        playsound('banana-piano/wav-piano-sound/f4.mp3')
+        playsound('wav-piano-sound/f4.mp3')
         print("Note played6!")
     elif  note == "seven":
-        playsound('banana-piano/wav-piano-sound/f-4.mp3')
+        playsound('wav-piano-sound/f-4.mp3')
         print("Note played7!")
     elif  note == "eight":
-        playsound('banana-piano/wav-piano-sound/g4.mp3')
+        playsound('wav-piano-sound/g4.mp3')
         print("Note played8!")
     elif  note == "nine":
-        playsound('banana-piano/wav-piano-sound/g-4.mp3')
+        playsound('wav-piano-sound/g-4.mp3')
         print("Note played9!")
     elif  note == "ten":
-        playsound('banana-piano/wav-piano-sound/a5.mp3')
+        playsound('wav-piano-sound/a5.mp3')
         print("Note played10!")
     elif  note == "eleven":
-        playsound('banana-piano/wav-piano-sound/a-5.mp3')
+        playsound('wav-piano-sound/a-5.mp3')
         print("Note played11!")
     elif  note == "twelve":
-        playsound('banana-piano/wav-piano-sound/b5.mp3')
+        playsound('wav-piano-sound/b5.mp3')
         print("Note played12!")
 
 def read_serial():
