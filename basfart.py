e9487e34676706075918f3719c8a70153a27162e
Mustafa <abou08923@gmail.com>
2023-03-30 10:13:36 +0200

    added fart mode

--- basfart.py
+++ basfart.py
@@ -8,42 +8,44 @@ ser = serial.Serial('COM3', 9600)  # replace 'COM3' with the name of your serial
 def play_sound(note):
     if note == "one":
         # play a note when touch sensor is touched
-        playsound('wav-piano-sound/c4.mp3')#dit hoort c4 te zijn
+        playsound('wav-piano-sound/wet_fart_1.wav')#dit hoort c4 te zijn
         print("Note played!")
     elif note == "two":
         # play a note when touch sensor is touched
-        playsound('wav-piano-sound/c-4.mp3')#dit hoort c-4 te zijn
+        playsound('wav-piano-sound/wet_fart_2.mp3')#dit hoort c-4 te zijn
         print("Note played2!")
     elif  note == "three":
-        playsound('wav-piano-sound/d4.mp3')#dit hoort d4 te zijn
+        playsound('wav-piano-sound/wet_fart_3.wav')#dit hoort d4 te zijn
         print("Note played3!")
     elif  note == "four":
-        playsound('wav-piano-sound/d-4.mp3')#d-4
+        playsound('wav-piano-sound/wet_fart_4.wav')#d-4
         print("Note played4!")
     elif  note == "five":
-        playsound('wav-piano-sound/e4.mp3')#e4
+        playsound('wav-piano-sound/wet_fart_5.mp3')#e4
         print("Note played5!")
     elif  note == "six":
-        playsound('wav-piano-sound/f4.mp3')
+        playsound('wav-piano-sound/wet_fart_6.wav')
         print("Note played6!")
     elif  note == "seven":
-        playsound('wav-piano-sound/f-4.mp3')
+        playsound('wav-piano-sound/wet_fart_7.wav')
         print("Note played7!")
     elif  note == "eight":
-        playsound('wav-piano-sound/g4.mp3')
+        playsound('wav-piano-sound/wet_fart_8.wav')
         print("Note played8!")
     elif  note == "nine":
-        playsound('wav-piano-sound/g-4.mp3')
+        playsound('wav-piano-sound/wet_fart_9.wav')
         print("Note played9!")
     elif  note == "ten":
-        playsound('wav-piano-sound/a5.mp3')
+        playsound('wav-piano-sound/wet_fart_10.wav')
         print("Note played10!")
     elif  note == "eleven":
-        playsound('wav-piano-sound/a-5.mp3')
+        playsound('wav-piano-sound/wet_fart_11.mp3')
         print("Note played11!")
     elif  note == "twelve":
-        playsound('wav-piano-sound/b5.mp3')
+        playsound('wav-piano-sound/wet_fart_12.mp3')
         print("Note played12!")
+
+
 
 def read_serial():
     while True:
